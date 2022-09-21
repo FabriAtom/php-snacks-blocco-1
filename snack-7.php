d1ca09e973e45f64800ec8afafa21c67ab18b698
FabriAtom <fabrizioromano20@gmail.com>
2022-09-21 15:11:46 +0200

    error snack 1-4

---
+++ snack-7.php
@@ -0,0 +1,26 @@
+<?php
+ 
+    $classe = [
+        'teachers' => [
+            [
+                'name' => 'Michele',
+                'lastname' => 'Papagni'
+            ],
+            [
+                'name' => 'Fabio',
+                'lastname' => 'Forghieri'
+            ]
+        ],
+        'pm' => [
+            [
+                'name' => 'Roberto',
+                'lastname' => 'Marazzini'
+            ],
+            [
+                'name' => 'Federico',
+                'lastname' => 'Pellegrini'
+            ]
+        ]
+    ];
+ 
+?>
